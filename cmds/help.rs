7fe021ba39150ff3b93eb88bcf030734d8b86880
Brat1shkaKERCIX <sasaslobodan174@gmail.com>
2026-05-16 10:01:33 +0300

    Create help.rs

---
+++ cmds/help.rs
@@ -0,0 +1,10 @@
+pub fn run() {
+    println!("Commands:");
+    println!(" help        - Show commands");
+    println!(" clear       - Clear console");
+    println!(" cmd         - Open CMD");
+    println!(" powershell  - Open PowerShell");
+    println!(" files       - Show files");
+    println!(" run APP     - Run app");
+    println!(" exit        - Exit shell");
+}
